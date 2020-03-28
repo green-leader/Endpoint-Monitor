9cb4dd4e04fbf82e89a9d0223f8fe59f8f26ab77
Sion F <github@sions.org>
2020-03-28 15:55:47 -0600

    Removed commented code

--- Monitor/cli.py
+++ Monitor/cli.py
@@ -1,12 +1,5 @@
 # cli.py
 import click
-# @click.command()
-# @click.argument('name')
-# @click.option('--greeting', '-g')
-# def main(name, greeting):
-#     click.echo("{}, {}".format(greeting, name))
-# if __name__ == "__main__":
-#     main()
 
 
 @click.group()
