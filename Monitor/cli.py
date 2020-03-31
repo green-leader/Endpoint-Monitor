225e31780976880a68359f4221b01d9e34f30c4d
Sion F <github@sions.org>
2020-03-30 22:48:09 -0600

    Added deletion of Specified URL

--- Monitor/cli.py
+++ Monitor/cli.py
@@ -50,11 +50,21 @@ def update(update):
 
 
 @main.command()
-@click.argument('delete', default='')
-def delete(delete):
+@click.argument('delete')
+def delete(delete, default=''):
     """
     Delete designated URL
     """
+    if delete == '':
+        click.echo(
+          "after delete keyword you need to entry the URL to delete", err=True)
+    b64 = base64.b64encode(delete.encode('ascii'))
+    try:
+        core.delete(b64.decode())
+    except Exception as err:
+        click.echo(
+          "There was an issue performing the deletion: %s" % repr(err),
+          err=True)
     pass
 
 
