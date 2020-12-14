b7eddeba06cfefb14b26de21931e61043f9f0dad
Sion F <github@sions.org>
2020-12-14 13:52:31 -0700

    Updated coverage for cli.py

--- tests/testutils.py
+++ tests/testutils.py
@@ -1,6 +1,6 @@
 '''Utilities to use within testing'''
 from unittest import mock
-
+import base64
 
 def get_fake_get(status_code, content):
     '''Fake response for requests.get to use in mock patching takes in status_c\
@@ -13,3 +13,14 @@ ode and content'''
         return m
 
     return fake_get
+
+
+def get_fake_listing():
+    fake_listing = dict()
+    pageEntry = dict()
+    URL = base64.b64encode(b'https://example.com')
+    pageEntry['URL'] = URL.decode()
+    pageEntry['name'] = 'TestInput'
+    pageEntry['hash'] = '1234'
+    fake_listing[URL.decode()] = pageEntry
+    return fake_listing
